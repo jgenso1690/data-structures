f69b6b0d07cc934a5807d40d61085f736318cee3
Benjamin Barnett <ben.barnett@gmail.com>
2020-03-25 21:23:45 -0700

    Implement functional-shared queue, begin stack

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,30 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var storage = {};
+  _.extend(storage, queueMethods);
+  return storage;
 };
 
-var queueMethods = {};
-
-
+var queueMethods = {
+  count: 0,
+  enqueue: function(value){
+    var count = this.count; // We needed a counter variable to point towards the _value_ contained by this.count, not to point at this.count itself
+    this[count] = value; // This line will assign the value to the _number_ that this.count currently contains
+    this.count++;
+  },
+  dequeue: function(){
+    var count = this.count;
+    var result = this['0'];
+    for (var i = 1; i < count; i++){
+      this[i - 1] = this[i];
+    }
+    if (count > 0){
+      this.count--;
+    }
+    return result;
+  },
+  size: function() {
+    return this.count;
+  }
+};
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,27 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var storage = {};
+  _.extend(storage, stackMethods);
+  return storage;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  count: 0,
+  push: function(value){
+    // this[count] = value;
+    // this.count++;
+  },
+  pop: function(){
+    // var result = this.count;
+    // if (this.count > 0) {
+    //   this.count--;
+    // }
+    // return result;
+  },
+  size: function(){
+    // return this.count;
+  }
+};
 
 
