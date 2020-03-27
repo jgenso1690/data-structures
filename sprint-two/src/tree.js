6910d0bf162e13f9cac817e40317b35bc89cfc15
jgenso1690 <jimenagensollen@gmail.com>
2020-03-27 18:05:57 -0400

    Tree addchild

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,16 +1,23 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
+  // input: any value
+  // output: a tree with methods from the treeMethods object
+  // each child may have children or not
+
+
 
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];  // fix me
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
-treeMethods.addChild = function(value) {
+treeMethods.addChild = function(value) {   // what object is to the left of the dot?   pointerC.addchild(e) -> var newChild
+  var newChild = Tree(value);  // pointerA.addchild(c)  ->  var newChild = Tree(c) -> pointerA.children.push(newChild)
+  this.children.push(newChild);  // currentTree = [treeObject that CONTAINS a]
 };
 
 treeMethods.contains = function(target) {
